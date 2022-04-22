c6995acdbda6d690617ff8d20ead0bad615cc661
Swetha Valiya Parambath <swetha.valiya.parambath@ibm.com>
2022-04-22 16:34:35 +0530

    First commit- swetha

---
+++ CucumberGitJenkins/src/test/java/stepdefinition/loginSteps.java
@@ -0,0 +1,19 @@
+package stepdefinition;
+
+import cucumber.api.java.en.Given;
+
+public class loginSteps {
+	
+
+@Given("User launches the application")
+public void user_launches_the_application() {
+  
+}
+
+@Given("Enter valid credentials")
+public void enter_valid_credentials() {
+
+}
+
+	
+}
---
+++ CucumberGitJenkins/target/test-classes/feature/login.feature
@@ -0,0 +1,8 @@
+
+Feature: Login feature
+  I want to login to my portal
+
+  Scenario: Login with valid credentials
+    Given User launches the application
+    And Enter valid credentials
+
